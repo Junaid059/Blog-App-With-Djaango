205b6dad36b6bba606628bc68defde98bb765015
junaid059 <junaidkhalid2001@gmail.com>
2026-06-19 23:28:16 +0500

    start

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-    def __str__(self):
-        return self.name
-
 
 
 class Category(models.Model):
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+from rest_framework.permissions import BasePermission, IsAdminUser,DjangoModelPermissionsOrAnonReadOnly
 
-# Create your views here.
+class CustomUserPermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in ['GET','POST','PUT','DELETE']:
+            return request.user and request.user.is_staff
+        return True
+
+class PostList(generics.ListCreateAPIView):
+    permission_classes = [CustomUserPermission]
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [CustomUserPermission]
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
